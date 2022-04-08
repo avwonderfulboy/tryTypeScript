fca07d43f170f0274764eba017b07a88deaea0fe
Anshuman Singh <avwonderfulboy@gmail.com>
2022-04-08 09:24:05 +0530

    tsc build

--- urlShort.ts
+++ urlShort.ts
@@ -1,18 +1,21 @@
-import { APIGatewayEvent, APIGatewayProxyEventQueryStringParameters } from "aws-lambda";
-import {DynamoDB,Lambda} from "aws-sdk";
-
+import {
+  APIGatewayEvent,
+  APIGatewayProxyEventQueryStringParameters,
+} from "aws-lambda";
+import { DynamoDB, Lambda } from "aws-sdk";
 
 const dynamodb = new DynamoDB({
   apiVersion: "2012-08-10",
   region: "us-east-1",
 });
 
-exports.handler = async (event:APIGatewayEvent) => {
+exports.handler = async (event: APIGatewayEvent) => {
   try {
     if (event.requestContext.httpMethod === "GET") {
-      const { short_url } = event.queryStringParameters as APIGatewayProxyEventQueryStringParameters;
+      const { short_url } =
+        event.queryStringParameters as APIGatewayProxyEventQueryStringParameters;
 
-      if (short_url?.length=== 0)
+      if (short_url?.length === 0)
         return {
           statusCode: 400,
           body: "Short url cannot be empty ",
@@ -42,7 +45,9 @@ exports.handler = async (event:APIGatewayEvent) => {
       };
     }
     if (event.requestContext.httpMethod === "POST") {
-      const { long_url }: { long_url: string } = JSON.parse(event.body as string);
+      const { long_url }: { long_url: string } = JSON.parse(
+        event.body as string
+      );
       if (long_url.length == 0) return { body: " url cannot be empty  " };
       let short_url =
         Math.random().toString(32).substring(2, 4) +
